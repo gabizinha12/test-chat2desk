4d914a26f976056806b7b54d648bcbe1e9cc2fc4
gabizinha12 <gabrielle.plima1965@gmail.com>
2021-01-05 17:41:06 -0300

    teste repositório

--- Database/config.js
+++ Database/config.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const config = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, {
+const config = new Sequelize(process.env.DATABASE, process.env.USER, {
   dialect: "mysql",
 });
 module.exports = config;
