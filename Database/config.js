efdd260d2862a4ffe46956acd4016bd95ff94c87
gabizinha12 <gabrielle.plima1965@gmail.com>
2021-01-05 17:51:48 -0300

    fixed env config and MySQL config

--- Database/config.js
+++ Database/config.js
@@ -2,7 +2,12 @@ const Sequelize = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const config = new Sequelize(process.env.DATABASE, process.env.USER, {
-  dialect: "mysql",
-});
+const config = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    dialect: "mysql",
+  }
+);
 module.exports = config;
