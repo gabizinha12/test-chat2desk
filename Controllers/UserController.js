a226adcb19d6c539097bdda681d4de7adf6c9d72
gabizinha12 <gabrielle.plima1965@gmail.com>
2021-01-07 18:36:11 -0300

    need to fix index and implement login

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -5,19 +5,28 @@ const bcrypt = require("bcrypt");
 module.exports = {
   async Create(req, res) {
     let { name, email, password } = req.body;
-    await User.findOne({ where: { email: email } }).then((user) => {
-      if (user === undefined) {
-        let salt = bcrypt.genSaltSync(10);
-        let hash = bcrypt.hashSync(password, salt);
+    await User.findOne({ where: { email: email } })
+      .then((users) => {
+        if (users === undefined) {
+          let salt = bcrypt.genSaltSync(10);
+          let hash = bcrypt.hashSync(password, salt);
 
-        User.create({
-          name: name,
-          email: email,
-          password: hash,
-        });
-      }
-      return res.json(user);
-    });
+          User.create({
+            name: name,
+            email: email,
+            password: hash,
+          });
+        } else if (
+          (req.body.email == undefined && null) ||
+          (req.body.password == undefined && null)
+        ) {
+          req.flash("error_msg", "Usuário inválido!");
+        }
+        return res.redirect("/");
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   },
 
   async Register(req, res) {
@@ -25,14 +34,14 @@ module.exports = {
   },
 
   async Read(req, res) {
-    User.findAll().then((users) => {
-      res.render("index");
+    await User.findAll().then((users) => {
+      res.render("index", { users: users });
     });
   },
   async Login(req, res) {
     let { name, email, password } = req.body;
 
-    User.findOne({ where: { email: email } }).then((user) => {
+    await User.findOne({ where: { email: email } }).then((user) => {
       if (user != undefined) {
         const correct = bcrypt.compareSync(password, user.password);
         if (correct) {
--- server.js
+++ server.js
@@ -3,13 +3,18 @@ dotenv.config({ path: "./env" });
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const express = require("express");
-const flash = require("req-flash");
+const flash = require("connect-flash");
 const connectionTest = require("./Database/connectionTest");
 const bodyParser = require("body-parser");
 const routes = require("./Routes/Routes");
 
 const server = express();
-
+server.use(flash());
+server.set("view engine", "ejs");
+server.set("views", "views");
+server.use(express.static("public"));
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(express.json());
 server.use(cookieParser());
 server.use(
   session({
@@ -19,12 +24,14 @@ server.use(
     cookie: { maxAge: 3000 },
   })
 );
-server.use(flash());
-server.set("view engine", "ejs");
-server.set("views", "views");
-server.use(express.static("public"));
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(express.json());
+
+// Middleware
+server.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.user = req.user || null;
+  next();
+});
 
 server.listen(3000 || process.env.PORT, () => {
   console.log("server is listening");
